19b9a08642e14cae016a07cbc2eb60009796dc64
Mike <mike.sadonis@student.howest.be>
2013-12-17 10:48:09 +0100

    aanmaken store app
    aanmaken en beginnen met de store app

---
+++ Festival/Common/ListConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace Festival.Common
+{
+    class ListConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            ObservableCollection<string> items = (ObservableCollection<string>)value;
+            StringBuilder builder = new StringBuilder();
+
+            foreach (var item in items)
+            {
+                builder.Append(item);
+                builder.Append("\r\n");
+            }
+
+            return builder.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
